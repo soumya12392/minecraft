9c318cb3a8bf4e474a803288309f39685ce7a319
soumya12392 <83289414+soumya12392@users.noreply.github.com>
2021-07-13 18:31:48 +0530

    Add files via upload
    Applied the code to upload all the blocks on respective keys.

--- main.js
+++ main.js
@@ -39,3 +39,91 @@ function new_image(get_image) {
         canvas.add(block_image_object);
     })
 }
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    var keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if (e.shiftKey == true && keyPressed == 80) {
+        console.log("P and shift pressed together");
+        block_image_width += 10;
+        block_image_height += 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+    if (e.shiftKey == true && keyPressed == 77) {
+        console.log("M and shift pressed together");
+        block_image_width -= 10;
+        block_image_height -= 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+    if (keyPressed == 38) {
+        up();
+        console.log("Up");
+    }
+
+    if (keyPressed == 39) {
+        right();
+        console.log("Right");
+    }
+
+    if (keyPressed == 37) {
+        left();
+        console.log("Left");
+    }
+
+    if (keyPressed == 40) {
+        down();
+        console.log("Down");
+    }
+
+    if (keyPressed == 84) {
+        new_image("trunk.jpg");
+        console.log("Trunk");
+    }
+
+    if (keyPressed == 68) {
+        new_image("dark_green.png");
+        console.log("Dark green");
+    }
+
+    if (keyPressed == 76) {
+        new_image("light_green.png");
+        console.log("Light green");
+    }
+
+    if (keyPressed == 71) {
+        new_image("ground.png");
+        console.log("Ground");
+    }
+
+    if (keyPressed == 87) {
+        new_image("wall.jpg");
+        console.log("Wall");
+    }
+
+    if (keyPressed == 89) {
+        new_image("yellow_wall.png");
+        console.log("Yellow wall");
+    }
+
+    if (keyPressed == 82) {
+        new_image("roof.jpg");
+        console.log("Roof");
+    }
+
+    if (keyPressed == 67) {
+        new_image("cloud.jpg");
+        console.log("Cloud");
+    }
+
+    if (keyPressed == 85) {
+        new_image("unique.png");
+        console.log("Unique");
+    }
+}
